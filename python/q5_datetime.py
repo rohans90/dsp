b14755e9721a7c672779aeb5b8b05ceced0b762c
rohans90 <rohans@umich.edu>
2016-08-07 16:15:04 +0530

    Update q5_datetime.py

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -1,5 +1,14 @@
 # Hint:  use Google to find python function
 
+from datetime import date
+
+d0 = date(1994, 1, 15)
+d1 = date(2015, 7, 14)
+
+delta = d1 - d0
+
+print (delta.days)
+
 ####a) 
 date_start = '01-02-2013'  
 date_stop = '07-28-2015'   
